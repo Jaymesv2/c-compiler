108b10bf2610a08d4fba0e92512ba92a68784440
Trent <ghastfilms613@gmail.com>
2024-05-09 18:11:01 -0500

    disabled grammar module

--- src/Main.hs
+++ src/Main.hs
@@ -1,6 +1,6 @@
 module Main where
 
-import Compiler.Parser.Grammar
+--import Compiler.Parser.Grammar
 import Compiler.Parser.Lexer
 
 import Data.Text.IO qualified as TIO
@@ -19,38 +19,6 @@ main =  do
   --x <- runEff $ runAlex source clike
   --let source = "//int func(int i) { int i; }"
   -- print source
-  runEff $ runAlex source printTokens
+  _ <- runEff $ runAlex source printTokens
   pure ()
-  --print x
- --where
-  -- let x =
-  --      "\
-  --      \ i32 main(i32 x) { \
-  --      \   string s = \"bob\"; \
-  --      \   char c = 'b'; \
-  --      \   i32 y = 99; \
-  --      \   return ( y - x + 5 ); \
-  --      \ } \
-  --      \ "
-  -- let x = "int i;" -- works
-  -- let x = "struct x {};"
-
-  {-
-  let plist = "int i"
-  let tokens = alexScanTokens plist
-  let tree = parameterList tokens
-  -}
-  {-
-  let decl = "i()"
-  let tokens = alexScanTokens decl
-  let tree = directDeclarator tokens
-  -}
-  -- let x = "int main(int argc, char** argv) { println(\"hello world\\n\"); }"
-  -- let x = "typedef struct j { int i; long q; } j_t; int main(int argc, char **argv) { println(\"hello world\");}"
-  -- let tokens = alexScanTokens x
-  -- let tree = clike tokens
-
-  --t1 = do
-    --print "hi"
-
 -- For withUtf8, see https://serokell.io/blog/haskell-with-utf8
